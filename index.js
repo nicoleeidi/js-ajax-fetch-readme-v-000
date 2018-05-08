56eee7fd90bec25f66fc14956644b296142f0441
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 23:37:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+  const token =  '04ae7bc8f20238e70725123a0916a5951ab1bfe0'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
