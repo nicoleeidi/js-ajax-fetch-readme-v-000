6f218ae5258f95790e50c2adcdf5fff1f4d0c5b4
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 23:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much.";
 
-  const token = 'YOUR_TOKEN_HERE'
+  const token =  04ae7bc8f20238e70725123a0916a5951ab1bfe0 
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
